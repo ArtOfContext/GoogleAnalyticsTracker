edfcfbc9fd335505e8ba46e47806a0a626e873b3
Tom Dunn <tdunn@artofcontext.com>
2014-09-08 11:06:05 -0400

    Log arguments of controller actions as google analytics custom variables in addition to logging custom variables that were set on the request. Sort by custom variable name so they are always filed to same google analytics slot.  Add placeholder custom variables when there is not an *Name request variable matching an *Id action variable (also to keep them always getting filed to same slot).

--- GoogleAnalyticsTracker.WebAPI2/ActionTrackingAttribute.cs
+++ GoogleAnalyticsTracker.WebAPI2/ActionTrackingAttribute.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
@@ -121,23 +123,68 @@ namespace GoogleAnalyticsTracker.WebApi2 {
 
         private void SetRequestPropertiesCustomVariables(HttpActionExecutedContext filterContext)
         {
+            int position = 1;
+            var customVars = new List<Tuple<string, string>>(5);
+
             // read any "custom variables" that have been set on the request
             object customVariablesObj;
             filterContext.Request.Properties.TryGetValue("AnalyticsCustomVariables", out customVariablesObj);
-            var customVariablesDictionary = customVariablesObj as Dictionary<string, string> ?? new Dictionary<string, string>();
+            var requestCustomVariablesDict = customVariablesObj as Dictionary<string, string> ?? new Dictionary<string, string>();
 
-            int position = 1;
+            // also will take custom variables from the controller action arguments
+            Dictionary<string, object> actionArgumentsDict = filterContext.ActionContext.ActionArguments;
+
+            customVars.AddRange(requestCustomVariablesDict.Keys.Select(key =>
+                new Tuple<string, string>(key, (requestCustomVariablesDict[key] ?? "").ToString(CultureInfo.InvariantCulture))));
+
+            customVars.AddRange(actionArgumentsDict.Keys.Select(key =>
+                new Tuple<string, string>(key, (actionArgumentsDict[key] ?? "").ToString())));
+
+            AddPlaceholdersForMissingRequestCustomVariables(requestCustomVariablesDict, actionArgumentsDict, customVars);
 
             Tracker.ClearCustomVariables();
 
-            foreach (var key in customVariablesDictionary.Keys)
+            foreach (var customVarPair in customVars.OrderBy(tuple => tuple.Item1))
             {
-                Tracker.SetCustomVariable(position++, key, customVariablesDictionary[key]);
+                Tracker.SetCustomVariable(position++, customVarPair.Item1, customVarPair.Item2);
 
                 // only 5 custom variables allowed in Google Analytics
                 if (position > 5)
                 {
                     break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// In the case that custom variables were recorded on the request.Properties inside controller actions
+        ///   and then the response was cached, and then later the response is served from cache:
+        /// Since came from cache, the action was not run this time around and custom vars were not recorded inside
+        ///   controller action.  We would like custom variables to always be logged in the same google analytics slot
+        ///   so record some placeholder custom variables
+        /// </summary>
+        private void AddPlaceholdersForMissingRequestCustomVariables(
+            Dictionary<string, string> requestCustomVariablesDict,
+            Dictionary<string, object> actionArgumentsDict,
+            List<Tuple<string, string>> customVars)
+        {
+            const string idKeySuffix = "Id";
+            const string nameKeySuffix = "Name";
+
+            var requestKeys = requestCustomVariablesDict.Keys;
+            var actionKeys = actionArgumentsDict.Keys;
+
+            // there should be an xyzName variable for each xyzId variable
+
+            foreach (var actionKey in actionKeys)
+            {
+                if (actionKey.EndsWith(idKeySuffix))
+                {
+                    string nameKey = actionKey.Substring(0, actionKey.Length - idKeySuffix.Length) + nameKeySuffix;
+                    if (requestKeys.All(s => s != nameKey))
+                    {
+                        customVars.Add(new Tuple<string, string>(nameKey + "Placeholder", "0"));
+                    }
                 }
             }
         }
